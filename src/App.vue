d8afa51cec83ed93472b8337d42a816466422898
ALSDX <alsdx@mail.ru>
2025-09-06 16:46:37 +0500

    todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <template>
-  <h1>home page</h1>
+  <div class="container">
+    <div class="app__container">
+      <AddTask @add-todo="addTodo"></AddTask>
+      <TodosContainer :activeTodos="activeTodos" :doneTodos="doneTodos" @toggle-todo="toggleTodo" @remove-todo="removeTodo"></TodosContainer>
+    </div>
+  </div>
 </template>
 
-  <script>
-    export default {
-      name: "App"
-    }
-  </script>
+<script setup>
+import AddTask from "@/components/AddTask.vue";
+import TodosContainer from "@/components/TodosContainer.vue";
+  import {ref, computed} from 'vue';
 
-<style scoped></style>
+  const todos = ref([]);
+
+  const addTodo = (title) => {
+    if (title.trim()) {
+      const todo = {
+        title,
+        id: Date.now(),
+        done: false
+      }
+      todos.value.push(todo);
+    }
+  }
+
+  const toggleTodo = (todo) => {
+    todo.done = !todo.done
+  }
+
+  const removeTodo = (todo) => {
+    todos.value = todos.value.filter((t) => t.id !== todo.id)
+  }
+
+  const activeTodos = computed(() => todos.value.filter((todo) => !todo.done));
+  const doneTodos = computed(() => todos.value.filter((todo) => todo.done));
+</script>
+
+<style scoped>
+  .container {
+    background-color: #0D0714;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .app__container {
+    background-color: #1D1825;
+    min-width: 20%;
+    min-height: 50%;
+    padding: 25px 30px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    color: white;
+    font-size: 15px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  }
+</style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="todo__add-task">
+    <input type="text" placeholder="add a new task" class="add-task__input" v-model="addTodoInputValue" maxlength="30">
+    <button @click="addTodo" class="add-task__btn">+</button>
+  </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+
+    const emit = defineEmits(['add-todo']);
+
+    const addTodoInputValue = ref("");
+
+    const addTodo = () => {
+        if (addTodoInputValue.value.trim()) {
+            emit('add-todo', addTodoInputValue.value.trim());
+            addTodoInputValue.value = ""
+        }
+    }
+</script>
+
+<style scoped>
+  .todo__add-task {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+  }
+      .add-task__input {
+        flex: 1;
+        padding: 4px 8px;
+        border-radius: 7px;
+        outline: none;
+        border: 2px solid #9e78cf;
+        background: none;
+        font-size: 12px;
+        color: white;
+        transition: all .4s ease;
+
+          &:focus {
+            border: 2px solid #8a44e6;
+          }
+      }
+      .add-task__btn {
+        padding: 2px 5px;
+        border-radius: 5px;
+        border: none;
+        outline: none;
+        background: #9e78cf;
+        cursor: pointer;
+        color: white;
+      }
+</style>
